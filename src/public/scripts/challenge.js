399db48eba2ecf9581d22d5cd28b90e82bfdfe87
Jeff Free <jefffreelance3@gmail.com>
2021-09-10 00:54:07 -0300

    Mejorado sistema de desafios

--- src/public/scripts/challenge.js
+++ src/public/scripts/challenge.js
@@ -1,59 +1,73 @@
 //DEFINO LOS DESAFIOS
 const challenge = [
-	"6 puntos con dollyo n2p",
-	"dos blitz n2p",
-	"la combinacion puño, puño, puño, dolyo 2 veces 3p",
-	"3 puntos con una patada zona alta 3p",
-	"un contragolpe con giro tip 2p",
-	"dos contragolpes con giro tip 3p",
-	"tres contragolpes con giro tip 5p",
-	"un contragolpe con giro gancho 2p",
-	"un contragolpe con giro gancho 3p",
-	"dos contragolpes con giro gancho 5p",
-	"tres contragolpes con giro gancho 5p",
-	"2 puntos con patada descendente 3p",
-	"4 puntos con patada descendente 4p",
-	"3 puntos con una patada descendente 4p",
-	"que el rival se detenga 3 veces por una yop 2p",
-	"que el rival reciba 3 advertencias por salida 3p",
-	"que el rival retroceda 3 veces por tus amagues 2p",
-	"un punteo con yop y pegar gancho a la cabeza 4p",
-	"un punteo con yop y pegar dolyo a la cabeza con la misma pierna 4p",
-	"un doble punteo con yop y pegar dolyo con la otra pierna 3p",
-	"un esquive por debajo de una yop 3p",
-	"tres contragolpes a una yop del rival 5p",
-	"tres contragolpes a un dollyo del rival 5p",
-	"convertir un punteo en una descendente que puntue 5p",
-	"terminar una pelea sin recibir un solo punto 5p",
-	"terminar una pelea habiendo conseguido mas de 10 puntos 5p",
-	"que el rival te haga 2 puntos al empezar el combate y luego ganar 3p",
-	"que el rival te haga 1 punto al empezar el combate y luego ganar 2p",
-	"que el rival te haga 3 puntos al empezar el combate y luego ganar 4p",
-	"que el rival te haga 4 puntos al empezar el combate y luego ganar 5p",
-	"que el rival te haga 5 puntos al empezar el combate y luego ganar 6p",
-	"una mariposa que puntue 3p",
-	"tres giros seguidos y que por lo menos uno conecte 4p",
-	"puntos con yop, dollyos y puños en la misma pelea 3p",
-	"un antcipo a un dolyo con blitz 2p",
-	"dos anticipos a dolyos con blitz 3p",
-	"tres anticipos a dolyos con blitz 4p",
-	"una yop al cabezal 2p",
-	"un golpe de nudillo descendente, con o sin salto 2p",
-	"un esquive a un blitz saltando hacia atrás con golpe de nudillo 4p",
-	"puntos solo con patadas a zona alta y ganar 5p",
-	"puntos solo con puños y ganar 5p",
+	{"6 puntos con dollyo n2p": 2},
+	{"dos blitz": 2},
+	{"la combinacion puño, puño, puño, dolyo 2 veces": 3},
+	{"3 puntos con una patada zona alta": 3},
+	{"un contragolpe con giro tip": 2},
+	{"dos contragolpes con giro tip": 3},
+	{"tres contragolpes con giro tip": 5},
+	{"un contragolpe con giro gancho": 2},
+	{"un contragolpe con giro gancho": 3},
+	{"dos contragolpes con giro gancho": 5},
+	{"tres contragolpes con giro gancho": 5},
+	{"2 puntos con patada descendente": 3},
+	{"4 puntos con patada descendente": 4},
+	{"3 puntos con una patada descendente": 4},
+	{"que el rival se detenga 3 veces por una yop": 2},
+	{"que el rival reciba 3 advertencias por salida": 3},
+	{"que el rival retroceda 3 veces por tus amagues": 2},
+	{"un punteo con yop y pegar gancho a la cabeza": 4},
+	{"un punteo con yop y pegar dolyo a la cabeza con la misma pierna": 4},
+	{"un doble punteo con yop y pegar dolyo con la otra pierna": 3},
+	{"un esquive por debajo de una yop": 3},
+	{"tres contragolpes a una yop del rival": 5},
+	{"tres contragolpes a un dollyo del rival": 5},
+	{"convertir un punteo en una descendente que puntue": 5},
+	{"terminar una pelea sin recibir un solo punto": 5},
+	{"terminar una pelea habiendo conseguido mas de 10 puntos": 5},
+	{"que el rival te haga 2 puntos al empezar el combate y luego ganar": 3},
+	{"que el rival te haga 1 punto al empezar el combate y luego ganar": 2},
+	{"que el rival te haga 3 puntos al empezar el combate y luego ganar": 4},
+	{"que el rival te haga 4 puntos al empezar el combate y luego ganar": 5},
+	{"que el rival te haga 5 puntos al empezar el combate y luego ganar": 6},
+	{"una mariposa que puntue": 3},
+	{"tres giros seguidos y que por lo menos uno conecte": 4},
+	{"puntos con yop, dollyos y puños en la misma pelea": 3},
+	{"un antcipo a un dolyo con blitz": 2},
+	{"dos anticipos a dolyos con blitz": 3},
+	{"tres anticipos a dolyos con blitz": 4},
+	{"una yop al cabezal": 2},
+	{"un golpe de nudillo descendente, con o sin salto": 2},
+	{"un esquive a un blitz saltando hacia atrás con golpe de nudillo": 4},
+	{"puntos solo con patadas a zona alta y ganar": 5},
+	{"puntos solo con puños y ganar": 5}
 ];
+
+function sumaPuntos(puntos) {
+
+	let puntosTotales = JSON.parse(localStorage.getItem("puntosTotales"));
+	let suma = parseInt(puntosTotales + puntos);
+	localStorage.setItem("puntosTotales", JSON.stringify(suma));
+
+
+	let puntosView = document.getElementById("puntos");
+	puntosView.innerHTML = `Hasta ahora has conseguido ${suma} puntos.`
+
+	}
 
   // ELIJO LA FORMA EN BASE A LA DECISION DEL USUARIO
 function elegirChallenge() {
-	const lista = challenge;
-	const result = document.getElementById("result");
-
-	let eleccion;
+	const listaDesafios = challenge;
+	const listaPuntos = challenge;
+	let desafioElegido;
 	let random = 0;
-	random = Math.floor(Math.random() * challenge.length);
-	eleccion = lista[random];
-	saveTask(eleccion);
+
+	random = Math.floor(Math.random() * listaDesafios.length);
+	desafioElegido = Object.keys(listaDesafios[random]);
+	puntosElegido = Object.values(listaPuntos[random]);
+
+	saveTask(desafioElegido, puntosElegido);
 	}
 
 // function borrar() {
@@ -61,15 +75,26 @@ function elegirChallenge() {
 // 	result.innerText = "";
 // }
 
-function saveTask (eleccion) {
+function saveTask (desafio, puntos) {
+
+	if (localStorage.getItem("puntos") === null) {
+		let puntajes = [];
+		puntajes.push(puntos);
+		localStorage.setItem("puntos", JSON.stringify(puntajes));
+	} else {
+		let puntajes = JSON.parse(localStorage.getItem("puntos"));
+		puntajes.push(puntos);
+		localStorage.setItem("puntos", JSON.stringify(puntajes));
+
+	}
 
 	if (localStorage.getItem("desafios") === null) {
 		let desafios = [];
-		desafios.push(eleccion);
+		desafios.push(desafio);
 		localStorage.setItem("desafios", JSON.stringify(desafios));
 	} else {
 		let desafios = JSON.parse(localStorage.getItem("desafios"));
-		desafios.push(eleccion);
+		desafios.push(desafio);
 		localStorage.setItem("desafios", JSON.stringify(desafios));
 
 	}
@@ -78,34 +103,46 @@ function saveTask (eleccion) {
 };
 
 function getTasks() {
-	let desafios = JSON.parse(localStorage.getItem("desafios"));
-	let challengesView = document.getElementById("desafios");
+	if (localStorage.getItem("desafios") != null) {
+		let desafios = JSON.parse(localStorage.getItem("desafios"));
+		let puntos = JSON.parse(localStorage.getItem("puntos"));
+		let challengesView = document.getElementById("desafios");
 
-	challengesView.innerHTML = "";
+		challengesView.innerHTML = "";
 
-	for(let i = 0; i < desafios.length; i++) {
-		let desafio = desafios[i];
+		for(let i = 0; i < desafios.length; i++) {
+			let desafio = desafios[i];
+			let punto = puntos[i];
 
-		challengesView.innerHTML += `<div class="card mb-4 text-center">
-		<div class="card-body">
-			<p>Tienes que hacer ${desafio}</p>
-			<a onclick='deleteTask("${desafio}")' class="btn btn-danger">Eliminar</a>
-			<a onclick='deleteTask("${desafio}")' class="btn btn-primary">Eliminar</a>
-		</div>
-		</div>`
+			challengesView.innerHTML += `<div class="card mb-4 text-center">
+			<div class="card-body">
+				<p>Tienes que hacer ${desafio}.</p>
+				<p><b>RECOMPENSA: ${punto} puntos</b></p>
+				<a onclick='endTask("${i}", "false")' class="btn btn-danger">Fallado</a>
+				<a onclick='endTask("${i}", "true")' class="btn btn-primary">Completado</a>
+			</div>
+			</div>`
+		}
 	}
 };
 
-function deleteTask(desafio) {
+function endTask(posicion, status) {
 	let desafios = JSON.parse(localStorage.getItem("desafios"));
-	for(let i = 0; i < desafios.length; i++) {
-		if (desafios[i] == desafio) {
-			desafios.splice(i, 1);
-		}
+	let puntos = JSON.parse(localStorage.getItem("puntos"));
+
+	if (status == "false") {
+		desafios.splice(posicion, 1);
+		puntos.splice(posicion, 1);
+	} else {
+		sumaPuntos(parseInt(puntos[posicion]))
+		desafios.splice(posicion, 1);
+		puntos.splice(posicion, 1);
 	}
 
 	localStorage.setItem("desafios", JSON.stringify(desafios));
+	localStorage.setItem("puntos", JSON.stringify(puntos));
 	getTasks();
 };
 
 getTasks();
+sumaPuntos(0);
