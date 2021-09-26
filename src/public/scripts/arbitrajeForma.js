9da8d0c4c9d366adc80cc3eaa5b84a0785d253a1
Jeff Free <jefffreelance3@gmail.com>
2021-09-25 22:58:29 -0300

    Arbitraje de forma añadido

--- src/public/scripts/arbitrajeForma.js
+++ src/public/scripts/arbitrajeForma.js
@@ -7,6 +7,10 @@ let blueCounterLvl2 = 0;
 let redCounterLvl3 = 0;
 let blueCounterLvl3 = 0;
 let fightStatus = "ended";
+let redUndoLvl2 = false
+let redUndoLvl3 = false
+let blueUndoLvl2 = false
+let blueUndoLvl3 = false
 
 function startMatch(level) {
     if (fightStatus == "ended") {
@@ -55,6 +59,66 @@ function startMatch(level) {
     }
 }
 
+function undoLvl2(color) {
+    if (color == "red" && redUndoLvl2) {
+        const red = document.getElementById("red");
+        if (redCounterLvl2 >= 5) {
+			redCounterLvl2 -= 2;
+		} else {redCounterLvl2 -= 1;
+		}
+        redTotal += 0.5;
+        red.innerText = `x${redCounterLvl2}`;
+		redUndoLvl2 = false
+
+		console.log(redTotal);
+        console.log(redCounterLvl2);
+    }
+
+    if (color == "blue" && blueUndoLvl2) {
+        const blue = document.getElementById("blue");
+        if (blueCounterLvl2 >= 5) {
+			blueCounterLvl2 -= 2;
+		} else {blueCounterLvl2 -= 1;
+		}
+        blueTotal += 0.5;
+        blue.innerText = `x${blueCounterLvl2}`;
+		blueUndoLvl2 = false
+
+		console.log(blueTotal);
+		console.log(blueCounterLvl2);
+    }
+}
+
+function undoLvl3(color) {
+    if (color == "red" && redUndoLvl3) {
+        const red = document.getElementById("red");
+        if (redCounterLvl3 >= 5) {
+			redCounterLvl3 -= 2;
+		} else {redCounterLvl3 -= 1;
+		}
+        redTotal += 0.5;
+        red.innerText = `x${redCounterLvl3}`;
+		redUndoLvl3 = false
+
+		console.log(redTotal);
+        console.log(redCounterLvl3);
+    }
+
+    if (color == "blue" && blueUndoLvl3) {
+        const blue = document.getElementById("blue");
+		if (blueCounterLvl3 >= 5) {
+			blueCounterLvl3 -= 2;
+		} else {blueCounterLvl3 -= 1;
+		}
+        blueTotal += 0.5;
+        blue.innerText = `x${blueCounterLvl3}`;
+		blueUndoLvl3 = false
+
+		console.log(blueTotal);
+		console.log(blueCounterLvl3);
+    }
+}
+
 function redLvl1(points) {
     redCounterLvl1 += 1;
     if (fightStatus == "running" && redCounterLvl1 <= 30) {
@@ -76,25 +140,12 @@ function redLvl2(points) {
             if (redCounterLvl2 >= 5) {
                 redCounterLvl2 = 5;
             }
-        } else if (points == 0.5) {
-            redCounterLvl2 -= 1;
-            if (redCounterLvl2 <= 0) {
-                redCounterLvl2 = 0;
-            }
         }
-
         if (fightStatus == "running" && redCounterLvl2 <= 4) {
-            if (
-                (redCounterLvl2 == 0 && points == 0.5) ||
-                (redCounterLvl2 == 5 && points == 0.5)
-            ) {
-                points = 0;
-            }
+			redUndoLvl2 = true;
             redTotal += points;
             if (redTotal <= 0) {
                 redTotal = 0;
-            } else if (redTotal >= 10) {
-                redTotal = 10;
             }
 
             red.innerText = `x${redCounterLvl2}`;
@@ -109,9 +160,6 @@ function redLvl3(points) {
     const red = document.getElementById("red");
 
     if (redTotal != 0) {
-
-		
-
         if (points == -0.5) {
             redCounterLvl3 += 1;
             if (redCounterLvl3 >= 5) {
@@ -125,12 +173,7 @@ function redLvl3(points) {
         }
 
         if (fightStatus == "running" && redCounterLvl3 <= 4) {
-            if (
-                (redCounterLvl3 == 0 && points == 0.5) ||
-                (redCounterLvl3 == 5 && points == 0.5)
-            ) {
-                points = 0;
-            }
+            redUndoLvl3 = true;
             redTotal += points;
             if (redTotal <= 0) {
                 redTotal = 0;
@@ -174,12 +217,7 @@ function blueLvl2(points) {
         }
 
         if (fightStatus == "running" && blueCounterLvl2 <= 4) {
-            if (
-                (blueCounterLvl2 == 0 && points == 0.5) ||
-                (blueCounterLvl2 == 5 && points == 0.5)
-            ) {
-                points = 0;
-            }
+            blueUndoLvl2 = true;
             blueTotal += points;
             if (blueTotal <= 0) {
                 blueTotal = 0;
@@ -189,7 +227,8 @@ function blueLvl2(points) {
 
             blue.innerText = `x${blueCounterLvl2}`;
 
-            console.log(blueTotal);
+            console.log(blueTotal);			
+            console.log(blueCounterLvl2);
         }
     }
 }
@@ -210,13 +249,7 @@ function blueLvl3(points) {
         }
 
         if (fightStatus == "running" && blueCounterLvl3 <= 4) {
-            if (
-                (blueCounterLvl3 == 0 && points == 0.5) ||
-                (blueCounterLvl3 == 5 && points == 0.5)
-            ) {
-                points = 0;
-            }
-
+            blueUndoLvl3 = true;
             blueTotal += points;
             if (blueTotal <= 0) {
                 blueTotal = 0;
@@ -240,29 +273,29 @@ function endLvl1() {
 		</div>
 		<div class="container">
 			<div class="row">
-				<div class="col-3 mb-2">
+				<div class="col-4 mb-2">
 					<div class="btn-group-vertical">
 
 						<button class="btn btn-danger btn-lg btn-block mt-3 mb-2" type="button"
 							onclick="redLvl2(-0.5)">-0.5</button>
 						<button class="btn btn-danger btn-lg btn-block mb-2" type="button"
-							onclick="redLvl2(0.5)">+0.5</button>
+							onclick="undoLvl2('red')">Deshacer</button>
 					</div>
 				</div>
-				<div class="col-3 mb-2">
+				<div class="col-2 mb-2">
 				<p id="red" class="text-center text-white fs-1 mt-3">x0</p>
 				</div>
-				<div class="col-3 mb-2">
+				<div class="col-4 mb-2">
 					<div class="btn-group-vertical">
 
 						<button class="btn btn-primary btn-lg btn-block mt-3 mb-2" type="button"
 							onclick="blueLvl2(-0.5)">-0.5</button>
 						<button class="btn btn-primary btn-lg btn-block mb-2" type="button"
-							onclick="blueLvl2(0.5)">+0.5</button>
+							onclick="undoLvl2('blue')">Deshacer</button>
 					</div>
 				</div>
 				
-				<div class="col-3 mb-2">
+				<div class="col-2 mb-2">
 				<p id="blue" class="text-center text-white fs-1 mt-3">x0</p>
 				</div>
 				<button class="btn btn-dark btn-lg btn-block" id="start-end-button" type="button"
@@ -279,28 +312,28 @@ function endLvl2() {
 		</div>
 		<div class="container">
 			<div class="row">
-				<div class="col-3 mb-2">
+				<div class="col-4 mb-2">
 					<div class="btn-group-vertical">
 
 						<button class="btn btn-danger btn-lg btn-block mt-3 mb-2" type="button"
 							onclick="redLvl3(-0.5)">-0.5</button>
 						<button class="btn btn-danger btn-lg btn-block mb-2" type="button"
-							onclick="redLvl3(0.5)">+0.5</button>
+							onclick="undoLvl3('red')">Deshacer</button>
 					</div>
 				</div>
-				<div class="col-3 mb-2">
+				<div class="col-2 mb-2">
 				<p id="red" class="text-center text-white fs-1 mt-3">x0</p>
 				</div>
-				<div class="col-3 mb-2">
+				<div class="col-4 mb-2">
 					<div class="btn-group-vertical">
 
 						<button class="btn btn-primary btn-lg btn-block mt-3 mb-2" type="button"
 							onclick="blueLvl3(-0.5)">-0.5</button>
 						<button class="btn btn-primary btn-lg btn-block mb-2" type="button"
-							onclick="blueLvl3(0.5)">+0.5</button>
+							onclick="undoLvl3('blue')">Deshacer</button>
 					</div>
 				</div>
-				<div class="col-3 mb-2">
+				<div class="col-2 mb-2">
 				<p id="blue" class="text-center text-white fs-1 mt-3">x0</p>
 				</div>
 				<button class="btn btn-dark btn-lg btn-block" id="start-end-button" type="button"
@@ -327,6 +360,10 @@ function endMatch() {
     blueCounterLvl2 = 0;
     redCounterLvl3 = 0;
     blueCounterLvl3 = 0;
+	redUndoLvl2 = false
+	redUndoLvl3 = false
+	blueUndoLvl2 = false
+	blueUndoLvl3 = false
     fightStatus = "ended";
 
     display.innerHTML = `<div class="container text-center" id="display">
